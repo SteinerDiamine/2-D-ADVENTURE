9d74e5052dd3753680bc6c6c2a7a84b56fc70bc5
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-02-24 16:52:43 +0530

    Working of character movement

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -43,29 +43,33 @@ public class Player extends Entity {
     }
 
     public void update() {
-        if (keyH.upPressed == true) {
-            direction = "up";
-            y -= speed;
-        } else if (keyH.downPressed == true) {
-            direction = "down";
-            y += speed;
-        } else if (keyH.leftPressed == true) {
-            direction = "left";
-            x -= speed;
+        if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
+            if (keyH.upPressed == true) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed == true) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed == true) {
+                direction = "left";
+                x -= speed;
 
-        } else if (keyH.rightPressed == true) {
-            direction = "right";
-            x += speed;
-        }
-        spriteCounter++;
-        if(spriteCounter> 12){
-            if(spriteNum == 1){
-                spriteNum=2;
-            } else if (spriteNum ==2) {
-                spriteNum =1;
+            } else if (keyH.rightPressed == true) {
+                direction = "right";
+                x += speed;
+            }
+            spriteCounter++;
+            if(spriteCounter> 12){
+                if(spriteNum == 1){
+                    spriteNum=2;
+                } else if (spriteNum ==2) {
+                    spriteNum =1;
 
-            }spriteCounter =0;
+                }spriteCounter =0;
+            }
+
         }
+
     }
         public void draw(Graphics2D g2) {
        /*  g2.setColor(Color.white);
